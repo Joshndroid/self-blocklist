4b23650cc1ab55e6c57d3dff17eb955ef5d8b779
Joshndroid <fmxjoshiewah@gmail.com>
2018-02-16 15:41:20 +1000

    Update

--- adblockinglists.sh
+++ adblockinglists.sh
@@ -1,6 +1,6 @@
 #Remove old list
 rm adaway.txt
-#grab the malwaredomains hosts files
+#Grab the malwaredomains hosts files
 wget http://mirror1.malwaredomains.com/files/justdomains
 #grab the coin-hive domains files
 wget https://raw.githubusercontent.com/Marfjeh/coinhive-block/master/domains
