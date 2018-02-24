98be3823a9a0f64fa7fc3713755a7cb28d790b9f
Joshndroid <fmxjoshiewah@gmail.com>
2018-02-24 16:10:12 +1000

    Lists : update blocklist

--- adblockinglists.sh
+++ adblockinglists.sh
@@ -1,5 +1,7 @@
-#Remove old list
+#Remove old list and files
 rm adaway.txt
+rm justdomains
+rm domains
 #Grab the malwaredomains hosts files
 wget http://mirror1.malwaredomains.com/files/justdomains
 #grab the coin-hive domains files
@@ -9,6 +11,4 @@ cat justdomains domains > merged
 #Generate easy adaway compatible file
 sed -e 's/^/127.0.0.1 /' merged > adaway.txt
 #remove temp downloaded domain and merged files
-rm justdomains
-rm domains
 rm merged
