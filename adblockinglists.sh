21dcd58746b9eb309a73fb21c7145f4fb14a51ee
Joshndroid <fmxjoshiewah@gmail.com>
2018-04-28 21:53:03 +1000

    Use http instead

--- adblockinglists.sh
+++ adblockinglists.sh
@@ -7,7 +7,7 @@ wget http://mirror1.malwaredomains.com/files/justdomains.zip
 unzip justdomains.zip justdomains
 rm justdomains.zip
 #grab the coin-hive domains files
-wget https://raw.githubusercontent.com/Marfjeh/coinhive-block/master/domains
+wget http://raw.githubusercontent.com/Marfjeh/coinhive-block/master/domains
 #merge the files together
 cat justdomains domains > merged
 #Generate easy adaway compatible file
