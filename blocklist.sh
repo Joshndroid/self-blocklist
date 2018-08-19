3a4fdb08167b6ff8b5020d0e74e34c4e98c3fcb3
Joshndroid <fmxjoshiewah@gmail.com>
2018-08-19 21:43:40 +1000

    testing - add blocklists for url

--- blocklist.sh
+++ blocklist.sh
@@ -1,5 +1,7 @@
+cd blocklist
 wget -O blocklist.zip "http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=zip"
 unzip blocklist.zip
 rm blocklist.zip
 mv *.txt blocklist.p2p
-
+cd ..
+mv blocklist/* .
